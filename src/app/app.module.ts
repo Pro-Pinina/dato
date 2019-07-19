88d55d97a7dc5e8621aac16b788659351286d537
Pro-Pinina <levantamientoinformacion8@gmail.com>
2019-07-19 14:09:52 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule, Route} from '@angular/router';
 
 
@@ -9,22 +12,28 @@ import { LoginComponent } from './login/login.component';
 import { EscritorioComponent } from './escritorio/escritorio.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { RegistroexcelentesComponent } from './registroexcelentes/registroexcelentes.component';
 
 
 const routes: Route[]= [
   {path: '',component: InicioComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'escritorio', component:EscritorioComponent}
+  {path: 'escritorio', component:EscritorioComponent},
+  {path: 'registroexcelentes', component:RegistroexcelentesComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    EscritorioComponent
+    EscritorioComponent,
+    RegistroexcelentesComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
